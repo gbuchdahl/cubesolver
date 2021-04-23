edae90d95b221b4d4e3d68abe35026c2d8980d97
Gabriel Buchdahl <gabriel@wanderlog.com>
2021-04-22 23:58:24 -0400

    ok for real this time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Cube = require("cubejs");
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 
 Cube.initSolver();
 
